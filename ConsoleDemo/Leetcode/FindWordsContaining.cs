ffa1b5607d6da204a965218f6f37652ffae751a8
deveras273 <deveras273@gmail.com>
2024-06-19 15:55:27 -0700

    Additonal Problems

---
+++ ConsoleDemo/Leetcode/FindWordsContaining.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class Solution6
+{
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        var result = new List<int>();
+        var arrLength = words.Length;
+        for (int i = 0; i < arrLength; i++)
+        {
+            var wordToTraverse = words[i];
+            var wordToLength = wordToTraverse.Length;
+            for (int j = 0; j < wordToLength; j++)
+            {
+                if (wordToTraverse[j] == x)
+                {
+                    result.Add(i);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ ConsoleDemo/Leetcode/MaximumWealth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Solution7
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        var result = int.MinValue;
+        var ans = 0;
+        var rows = accounts.Length;
+        var cols = accounts[0].Length;
+        for (int i = 0; i < rows; i++)
+        {
+            ans = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                ans = ans + accounts[i][j];
+            }
+            result = Math.Max(result, ans);
+        }
+        return result;
+
+
+    }
+}
