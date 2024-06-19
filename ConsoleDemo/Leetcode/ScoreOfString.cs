f69bc467866a43f1a67a7c550aef2720566469a2
deveras273 <deveras273@gmail.com>
2024-06-19 16:18:46 -0700

    Additional Problems

---
+++ ConsoleDemo/Leetcode/ScoreOfString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Solution8
+{
+    public int ScoreOfString(string s)
+    {
+        var stringLength = s.Length;
+        var result = 0;
+        for (int i = 1; i < stringLength; i++)
+        {
+            result = result + Math.Abs((int)(s[i] - s[i - 1]));
+        }
+        return result;
+    }
+}
